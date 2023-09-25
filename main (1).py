616efcd7252846bf0deeefac5142fb0c2b3a4859
asper104c22ug104cap019 <144328313+asper104c22ug104cap019@users.noreply.github.com>
2023-09-25 15:48:19 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,23 +1,10 @@
-def sort_students(student_list):
-    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
-    return sorted_students
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
 
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
+number = 2
+res = fact_rec(number)
 
-# Test with different input lists of students
-students_list = [
-    Student("John", "2021001", 3.9),
-    Student("Jane", "2021002", 3.7),
-    Student("Alice", "2021003", 3.8),
-    Student("Bob", "2021004", 3.6)
-]
-
-sorted_students = sort_students(students_list)
-
-# Print the sorted list of students
-for student in sorted_students:
-    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+print("The factorial of {} is {}".format(number,res))
--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-def linear_search_product(products, target_product):
-
-    indices = []
-
-    for index, product in enumerate(products):
-
-        if product == target_product:
-
-            indices.append(index)
-
-    return indices
-# Sample list of products
-
-product_list = ["apple", "banana", "orange", "apple", "grape", "apple"]
-
-
-
-# Target product to search for
-
-target_product = "apple"
-
-
-
-# Call the function
-
-result = linear_search_product(product_list, target_product)
-
-
-
-# Print the result
-
-print(result)
+year = int(input("Enter year to be checked: "))
+ 
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("The year is a leap year!")
+        else:
+            print("The year is not a leap year!")
+    else:
+        print("The year is a leap year!")
+else:
+    print("The year is not a leap year!")
