7ee6bd6ab6913175bcc7a754b76b956b7e75331d
asper104c22ug104cap019 <144328313+asper104c22ug104cap019@users.noreply.github.com>
2023-09-25 16:00:44 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,23 +1,25 @@
-def sort_students(student_list):
-    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
-    return sorted_students
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
 
-# Test with different input lists of students
-students_list = [
-    Student("John", "2021001", 3.9),
-    Student("Jane", "2021002", 3.7),
-    Student("Alice", "2021003", 3.8),
-    Student("Bob", "2021004", 3.6)
-]
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
 
-sorted_students = sort_students(students_list)
 
-# Print the sorted list of students
-for student in sorted_students:
-    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+
+# Testing the Player, Batsman, and Bowler classes
+if __name__ == "__main__":
+    player = Player()
+    player.play()  # Output: The player is playing cricket.
+
+    batsman = Batsman()
+    batsman.play()  # Output: The batsman is batting.
+
+    bowler = Bowler()
+    bowler.play()  # Output: The bowler is bowling.
--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-def linear_search_product(products, target_product):
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-    indices = []
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited {amount} units. New balance: {self.__account_balance}")
+        else:
+            print("Deposit amount must be greater than zero.")
 
-    for index, product in enumerate(products):
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew {amount} units. New balance: {self.__account_balance}")
+        else:
+            print("Withdrawal amount must be greater than zero and less than or equal to the account balance.")
 
-        if product == target_product:
-
-            indices.append(index)
-
-    return indices
-# Sample list of products
-
-product_list = ["apple", "banana", "orange", "apple", "grape", "apple"]
-
-
-
-# Target product to search for
-
-target_product = "apple"
-
-
-
-# Call the function
-
-result = linear_search_product(product_list, target_product)
+    def display_balance(self):
+        print(f"Account Holder: {self.__account_holder_name}")
+        print(f"Account Number: {self.__account_number}")
+        print(f"Account Balance: {self.__account_balance}")
 
 
+# Testing the BankAccount class
+if __name__ == "__main__":
+    account = BankAccount("1234567890", "John Doe", 1000)
 
-# Print the result
+    account.display_balance()  # Should display the initial balance
 
-print(result)
+    account.deposit(500)  # Depositing 500 units
+    account.withdraw(200)  # Withdrawing 200 units
+
+    account.display_balance()  # Should display the updated balance after transactions
